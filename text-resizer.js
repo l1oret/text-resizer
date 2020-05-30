45c18a5f04e993ea5674d7596b2adc8b9f05fbf3
l1oret <lloret.joseluis@gmail.com>
2020-05-30 21:29:01 +0200

    Refactor

--- text-resizer.js
+++ text-resizer.js
@@ -1,93 +1,183 @@
 (function (global) {
-  "use strict";
+  'use strict';
 
+  /**
+   * Resizer
+   */
+  const Resizer = {
+    /**
+     * Initializes the Resizer tool
+     * @constructs Resizer
+     * @param {object} heading
+     * @param {number} containerWidth
+     * @param {number} min
+     * @param {number} max
+     * @returns {Resizer}
+     */
+    init: function (heading, containerWidth, min, max) {
+      this.containerWidth = containerWidth;
+      this.heading = heading;
+      this.max = max;
+      this.min = min;
+      this.fontSize = this.getFontSize();
+
+      return this;
+    },
+
+    /**
+     * Get the heading's font-size
+     * @returns {void}
+     */
+    getFontSize() {
+      var style = window.getComputedStyle(this.heading, null);
+      return Math.round(parseFloat(style.getPropertyValue('font-size')));
+    },
+
+    /**
+     * Resize the heading until fit into its container
+     * @returns {void}
+     */
+    resize: function () {
+      this.updateFont();
+
+      if (this.isResizable()) {
+        this.resize();
+      }
+    },
+  };
+
+  /**
+   * Increases heading's font-size
+   * @extends Resizer
+   */
+  const Increase = Object.create(Resizer);
+
+  /**
+   * Set the heading font-size style
+   * @returns {void}
+   */
+  Increase.updateFont = function () {
+    this.fontSize++;
+    this.heading.style.fontSize = `${this.fontSize}px`;
+  };
+
+  /**
+   * Checks whether or not the heading font-size is increasable based on its
+   * container and the max font-size
+   * @returns {void}
+   */
+  Increase.isResizable = function () {
+    return (
+      this.heading.clientWidth < this.containerWidth &&
+      this.fontSize + 1 < this.max
+    );
+  };
+
+  /**
+   * Decreaes heading's font-size
+   * @extends Resizer
+   */
+  const Decrease = Object.create(Resizer);
+
+  /**
+   * Set the heading font-size style
+   * @returns {void}
+   */
+  Decrease.updateFont = function () {
+    this.fontSize--;
+    this.heading.style.fontSize = `${this.fontSize}px`;
+  };
+
+  /**
+   * Checks whether or not the heading font-size is decreasable based on its
+   * container and the min font-size
+   * @returns {void}
+   */
+  Decrease.isResizable = function () {
+    return (
+      this.heading.clientWidth > this.containerWidth &&
+      this.fontSize - 1 > this.min
+    );
+  };
+
+  /**
+   * Resizer actions
+   */
+  const actions = [Decrease, Increase];
+
+  /**
+   * Default optinos
+   */
+  const defaults = {
+    containerClass: '.tr-container',
+    headingClass: '.tr-heading',
+    max: 300,
+    min: 20,
+  };
+
+  /**
+   * Timeout timer
+   */
   let timer = 0;
 
   /**
-   * Define the constructor to instantiate the resizer
-   * @constructor
-   * @param {Object} options
+   * Iterates over each heading and resize it
+   *
+   * @param {object} options Custom options
+   * @returns {void}
    */
-  function TextResizer() {
-    global.addEventListener("resize", resizer, false);
-    global.addEventListener("orientationchange", resizer, false);
-
-    resizer();
-  }
-
-  /**
-   * Decrease heading font size
-   * @param {Object} heading
-   * @param {Number} containerWidth
-   */
-  function decreaseFontSize(heading, containerWidth) {
-    const fontSize = getFontSize(heading);
-    heading.style.fontSize = fontSize - 1 + "px";
-
-    if (heading.clientWidth > containerWidth && fontSize - 1 > 21) {
-      decreaseFontSize(heading, containerWidth);
-    }
-  }
-
-  function getFontSize(heading) {
-    var style = window.getComputedStyle(heading, null);
-    return Math.round(parseFloat(style.getPropertyValue("font-size")));
-  }
-
-  /**
-   * Increase heading font size
-   * @param {Object} heading
-   * @param {Number} containerWidth
-   */
-  function increaseFontSize(heading, containerWidth) {
-    const fontSize = getFontSize(heading);
-    heading.style.fontSize = fontSize + 1 + "px";
-
-    if (heading.clientWidth < containerWidth && fontSize + 1 < 300) {
-      increaseFontSize(heading, containerWidth);
-    }
-  }
-
-  /**
-   * Increase/Decrease heading font size
-   * @param {Boolean} operation
-   */
-  function resize(operation) {
-    return operation ? increaseFontSize : decreaseFontSize;
-  }
-
-  /**
-   * Resize headings
-   */
-  function resizer() {
+  function resizeText(options) {
     if (timer) {
-      window.clearTimeout(timer);
+      global.clearTimeout(timer);
     }
 
-    timer = window.setTimeout(function () {
-      let headings = document.querySelectorAll(".tr-heading");
+    // Resize only when resize event "has finished"
+    timer = global.setTimeout(function () {
+      let headings = document.querySelectorAll(options.headingClass);
 
       for (const heading of headings) {
-        let container = heading.closest(".tr-container");
+        let container = heading.closest(options.containerClass);
 
         if (container) {
-          let containerWidth = container.clientWidth;
-          let headingWidth = heading.clientWidth;
-
-          resize(headingWidth < containerWidth)(heading, containerWidth);
+          actions[+(heading.clientWidth < container.clientWidth)]
+            .init(heading, container.clientWidth, options.min, options.max)
+            .resize();
         }
       }
     }, 100);
   }
 
-  /** Expose headings */
-  if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
+  /**
+   * @constructor
+   *
+   * Define the constructor to instantiate the TextResizer
+   *
+   * @param {object} options Custom options
+   * @param {string} options.containerClass Container class selector
+   * @param {string} options.headingClass Heading class selector
+   * @param {string} options.max Maximun font-size
+   * @param {string} options.min Minimun font-size
+   * @returns {void}
+   */
+  function TextResizer({
+    containerClass = defaults.containerClass,
+    headingClass = defaults.headingClass,
+    max = defaults.max,
+    min = defaults.min,
+  } = {}) {
+    global.addEventListener('resize', resizeText, false);
+    global.addEventListener('orientationchange', resizeText, false);
+    resizeText({ containerClass, headingClass, min, max });
+  }
+
+  // Expose TextResizer
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
     module.exports = TextResizer;
-  } else if (typeof define === "function" && define.amd) {
-    define("TextResizer", [], function () {
+  } else if (typeof define === 'function' && define.amd) {
+    define('TextResizer', [], function () {
       return TextResizer;
     });
-  } else if (typeof global === "object") {
+  } else if (typeof global === 'object') {
     global.TextResizer = TextResizer;
   }
-})(typeof global !== "undefined" ? global : window);
+})(typeof global !== 'undefined' ? global : window);
